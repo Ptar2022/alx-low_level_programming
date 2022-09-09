c3ab6c482079648b82fe65e148bf0c9e85a277b9
Ptar2020 <peterolwande@gmail.com>
2022-09-09 23:20:15 +0300

    Format indentation

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,7 +13,7 @@ int main(void)
 	{
 		for (i = '0'; i <= '9'; i++)
 		{
-		  for (k = '0'; k <= '9'; k++)
+			for (k = '0'; k <= '9'; k++)
 			{
 				if (c < i && i < k)
 				{
@@ -23,10 +23,9 @@ int main(void)
 					if (c != '7')
 					{
 						putchar(',');
-
 						putchar(' ');
 					}
-				}
+				}	
 			}
 		}
 	}
