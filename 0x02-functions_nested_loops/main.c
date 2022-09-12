c445b89b84dc0e0f66a12cf8360f1c90ad1d1071
Ptar2020 <peterolwande@gmail.com>
2022-09-12 13:12:03 +0300

    0-putchar

---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0.
+ */
+
+int main()
+{
+  /**int c = 5;*/
+  /**int a = 6;*/
+  /** int b = 5;*/
+  /** print_alphabet();*/
+  /** print_alphabet_x10();*/
+  /** _islower(c);*/
+  /** _isalpha(c);*/
+  /**print_sign(c);*/
+  /**add(a,b);*/
+  /**_abs(a);*/
+  /**print_last_digit(a);*/
+  return 0;
+}
+
+
