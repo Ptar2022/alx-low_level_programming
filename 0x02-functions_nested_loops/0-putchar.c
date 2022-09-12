d02892d71dfac194bd06efca14c8bf9f7bd06f95
Ptar2020 <peterolwande@gmail.com>
2022-09-12 14:12:52 +0300

    x10

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/*
+* main - the entry point
+*
+* Return 0 for success
+*/
+
 int main(void)
 {
     _putchar('_');
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/*
+* print_alphabet_x10 - Prints the alphabet 10 times to the stdout
+* @c:The alphabet characters
+* Return 0 on success
+*/
+
+void print_alphabet_x10(void)
+{
+    char c;
+    int i = 0;
+
+    while (i <= 10)
+    {
+        for (c = 'a'; c <= 'z'; c++)
+        {
+            _putchar(c);
+        }
+        _putchar('\n');
+        i++;
+    }
+}
