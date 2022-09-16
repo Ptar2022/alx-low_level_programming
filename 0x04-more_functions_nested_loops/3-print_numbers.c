50b01b50a11acfa09c2b0cafd15ec74bed1f8ebc
Ptar2020 <peterolwande@gmail.com>
2022-09-16 09:36:42 +0300

    3-print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * print_numbers - Prints numbers 0 to 9
+ *
+ * Return: Nothing
+ */
+
+void print_numbers(void)
+{
+	int i;
+	
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+}
