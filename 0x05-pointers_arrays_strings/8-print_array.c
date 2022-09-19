29cdb1dc4dc094c16f27eb350caa09cd735d4fb0
Ptar <peterolwande@gmail.com>
2022-09-19 16:08:17 +0300

    0-array_print

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - prints the items in an array
+ * @a:The digits printed
+ * @n:The number of digits
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+    int i = 0;
+
+    while (i < n)
+    {
+        printf("%i", a[i]);
+        if (i != n -1)
+        {
+            printf(", ");
+        }
+        i = i + 1;
+    }
+}
