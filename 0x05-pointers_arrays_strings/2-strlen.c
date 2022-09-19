878598d57282ede7d02d7dbba3476cf8dac83738
Peter Olwande <makonalividza@gmail.com>
2022-09-19 10:31:51 +0300

    add

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strlen - Checks the length of a line
+ * @s: The character checking length
+ * Return: Integer length of a line
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	while (s[i] != '\0')
