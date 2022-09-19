e0031dfdd6a1dd1db8803a92802e80f9fa6d9888
Ptar <peterolwande@gmail.com>
2022-09-19 14:33:07 +0300

    9-strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strcpy - Copies the string
+ * @dest: The destination
+ * @src: The source
+ * Return: A character pointer
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+
+}
