c2acf7a8a43456da01e9464ef94f5cc97ae59953
Ptar <peterolwande@gmail.com>
2022-09-19 16:28:16 +0300

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,13 +9,13 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-    int i = 0;
+	int i = 0;
 
-    while (src[i] != '\0')
-    {
-        dest[i] = src[i];
-        i = i + 1;
-    }
-    dest[i++] = '\0';
-    return dest;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i = i + 1;
+	}
+	dest[i++] = '\0';
+	return dest;
 }
