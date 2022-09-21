f13c96f18fbac86ee20e5aa5909c6c475f8a9218
Peter Olwande <makonalividza@gmail.com>
2022-09-21 11:18:27 +0300

    101-crackme

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -5,20 +5,47 @@
 
 /**
  * main - Generates the password
- * @characters: The characters to pick password from
+ * @numbers: The characters to pick password from
+ * @uppers: The characters to pick password from
+ * @lowers: The characters to pick password from
+ * @symbols: The characters to pick password from
  * @password: The generated password
  * Return: 0 if success, 1 otherwise 
  */
 int main(void)
 {
-    char characters [] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+";
+    char symbols[] = "!@#$%^&*()";
+    char numbers[] = "0123456789";
+    char lowers[] = "abcdefghijklmnopqrstuvwxyz";
+    char uppers[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    //char characters [] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+";
     char password[10];
     int i;
 
     srand((unsigned int) time(NULL));
     for (i = 0; i < 10; i++)
     {
-        password[i] = characters[rand() % strlen(characters)];
+        int ran_type = rand() % 4;
+
+        if (ran_type == 0)
+        {
+            password[i] = symbols[rand() % strlen(symbols)];
+        }
+        else if (ran_type == 1)
+        {
+            password[i] = numbers[rand() % strlen(numbers)];
+        }
+        else if (ran_type == 2)
+        {
+            password[i] = lowers[rand() % strlen(lowers)];
+        }
+        else
+        {
+            password[i] = uppers[rand() % strlen(uppers)];
+        }
+        //password[i] = characters[rand() % strlen(characters)];
+        
         printf("%c", password[i]);
     }
     return (0);
