1a9e0fe3c8a36db84b0e1512711b4bc56499a0bb
Peter Olwande <makonalividza@gmail.com>
2022-09-21 11:30:49 +0300

    Update keygen

---
+++ 0x05-pointers_arrays_strings/101-keygen2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+/**
+ * main - Generates the password
+ * @characters: The characters to pick password from
+ * @password: The generated password
+ * Return: 0 if success, 1 otherwise 
+ */
+int main(void)
+{
+    char characters [] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+";
+    char password[10];
+    int i;
+
+    srand((unsigned int) time(NULL));
+    for (i = 0; i < 10; i++)
+    {
+        password[i] = characters[rand() % strlen(characters)];
+        printf("%c", password[i]);
+    }
+    return (0);
+}
