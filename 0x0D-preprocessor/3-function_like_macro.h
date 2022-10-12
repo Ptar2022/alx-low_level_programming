d0ac027cf2da9cb94b72f32ca0b407ce842f4175
Ptar2020 <peterolwande@gmail.com>
2022-10-12 20:50:27 +0300

    Updates 2

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef function_like_macro
-#define function_like_macro
+#ifndef _function_like_macro_h_
+#define _function_like_macro_h_
 
-#define ABS(x) x < 0 ? x * -1 : x
+#define ABS(x) (((x) < (0)) ? ((x) * (-1)) : (x))
 
-#endif
+#endif /* _function_like_macro_h_ */
