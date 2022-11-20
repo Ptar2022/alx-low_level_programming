35ba8b38ab06cc8eb7c755c435b539b49d6a997d
Ptar2020 <peterolwande@gmail.com>
2022-11-20 14:32:40 +0300

    Corrections

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,24 +10,25 @@
   */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int a = 0, b = 0;
+int a = 0, b = 0;
 
-	while (src[b])
-	{
-		b++;
-	}
-
-	while (a < n && src[a])
-	{
-		dest[a] = src[a];
-		a++;
-	}
-
-	while (a < n)
-	{
-		dest[a] = '\0';
-	a++;
-	}
-
-	return (dest);
+while (src[b])
+{
+b++;
 }
+
+while (a < n && src[a])
+{
+dest[a] = src[a];
+a++;
+}
+
+while (a < n)
+{
+dest[a] = '\0';
+a++;
+}
+
+return (dest);
+}
+
